fd8d873e517669d218be94eccfa684d9ed07c514
Farmasi Attila <attila.farmasi@dpdgroup.com>
2023-01-30 15:21:15 +0100

    Initial commit

---
+++ src/Context.js
@@ -0,0 +1,60 @@
+import React, { createContext, useState, useEffect } from "react";
+
+const Context = createContext();
+
+function ContextProvider(props) {
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAllPhotos(data));
+  }, []);
+
+  function toggleFavorite(id) {
+    const updatedArray = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return {
+          ...photo,
+          isFavorite: !photo.isFavorite,
+        };
+      }
+      return photo;
+    });
+
+    setAllPhotos(updatedArray);
+  }
+
+  function addToCart(newItem) {
+    setCartItems((prevItems) => [...prevItems, newItem]);
+  }
+
+  function removeFromCart(id) {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function emptyCart() {
+    setCartItems([]);
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        toggleFavorite,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        emptyCart,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+}
+
+export { Context, ContextProvider };
---
+++ src/pages/Cart.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
+
+function Cart() {
+  const [buttonText, setButtonText] = useState("Place Order");
+  const { cartItems, emptyCart } = useContext(Context);
+
+  const cartItemElement = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
+
+  const totalCost = cartItems.length * 5.99;
+  const totalCostDisplay = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  function placeOrder() {
+    setButtonText("Ordering...");
+    setTimeout(() => {
+      console.log("Order placed!");
+      setButtonText("Place Order");
+      emptyCart();
+    }, 3000);
+  }
+
+  return (
+    <main className="cart-page">
+      <h1>Check out</h1>
+      {cartItemElement}
+      <p className="total-cost">Total: {totalCostDisplay}</p>
+      {cartItems.length > 0 ? (
+        <div className="order-button">
+          <button onClick={placeOrder}>{buttonText}</button>
+        </div>
+      ) : (
+        <p>You have no items in your cart.</p>
+      )}
+    </main>
+  );
+}
+
+export { Cart };
